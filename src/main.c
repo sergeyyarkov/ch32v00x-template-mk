9a319bf9a52e5d82c07071169e16768ff54b6133
sergeyyarkov <serzh.yarkov@gmail.com>
2026-09-14 23:05:58 +0300

    blink prog

--- src/main.c
+++ src/main.c
@@ -1,29 +1,24 @@
 #include "../platform/ch32v00x/debug.h"
+#include "ch32v00x_gpio.h"
 
 int main(void) {
+  SystemInit();
+
   GPIO_InitTypeDef GPIO_InitStructure = {0};
 
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
 
   Delay_Init();
-  Delay_Ms(1000);
-  Delay_Ms(1000);
-#if (SDI_PRINT == SDI_PR_OPEN)
-  SDI_Printf_Enable();
-#else
-  USART_Printf_Init(115200);
-#endif
-  printf("SystemClk:%d\r\n", SystemCoreClock);
-  printf("ChipID:%08x\r\n", DBGMCU_GetCHIPID());
-
 
   while (1) {
-    Delay_Ms(1000);
-    GPIOA->OUTDR ^= GPIO_Pin_1;
+    GPIO_ResetBits(GPIOD, GPIO_Pin_4);
+    Delay_Ms(500);
+    GPIO_SetBits(GPIOD, GPIO_Pin_4);
+    Delay_Ms(500);
   }
 }
